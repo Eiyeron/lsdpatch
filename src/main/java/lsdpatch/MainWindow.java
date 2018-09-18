7c76c0fcd5f41439a32f28e8cc78645f7b9996b3
Eiyeron Fulmincendii <eiyeron@gmail.com>
2018-09-18 13:57:50 +0200

    Started making a main window instead of kit editor

---
+++ src/main/java/lsdpatch/MainWindow.java
@@ -0,0 +1,11 @@
+package lsdpatch;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    private final JButton kitEditorButton = new JButton("Kit Editor");
+    private final JButton paletteEditorButton = new JButton("Palette Editor");
+    private final JButton fontEditorButton = new JButton("Font Editor");
+    private final JButton openRomButton = new JButton("Open ROM");
+    private final JButton saveRomButton = new JButton("Save ROM");
+}
